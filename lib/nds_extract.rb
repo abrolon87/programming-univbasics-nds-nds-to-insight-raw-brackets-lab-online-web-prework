f5107a48c60318acc36ab10e205f7108d8840bcc
Amanda Rolon <abrolon87@gmail.com>
2019-11-13 15:56:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,37 +1,11 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+
+
+
+
 def directors_totals(nds)
-  index = 0
-  while index < nds.length do
-    puts "Row #{index} has #{nds[index]} columns"
-
-    column_index = 0
-    while column_index < nds[index].length do
-      coord = "#{index}, #{column_index}"
-      inner_len = nds[index][column_index].length
-      puts "\tCoordinate [#{coord}] points to an #{nds[index][column_index].class} of length #{inner_len}"
-
-      inner_index = 0
-      while inner_index < inner_len do
-        puts "\t\t (#{coord}, #{inner_len}) is: #{nds[index][column_index][inner_index]}"
-        inner_index += 1
-      end
-
-      column_index +=1
-    end
-
-    index += 1
-    # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-
-
- end
-
-
-```def directors_totals(nds)
   director_index = 0
   totals = {}
 
@@ -49,4 +23,4 @@ def directors_totals(nds)
   end
 
   totals
-end```
+end
